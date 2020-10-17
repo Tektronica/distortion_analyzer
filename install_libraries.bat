bb10f42b7a1b12c87dad7d878862ee705a1c4764
Tektronica <9472267+Tektronica@users.noreply.github.com>
2020-10-16 21:25:37 -0700

    First commit. Batch file to run gui easily.

---
+++ install_libraries.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+python -m pip install --user numpy scipy matplotlib pandas PyYAML wxPython PyVISA
+
+ECHO.
+ECHO Installation of python libraries complete!
+PAUSE
---
+++ run_gui.bat
@@ -0,0 +1,7 @@
+:: https://stackoverflow.com/a/8046515/3382269
+:: http://www.dostips.com/forum/viewtopic.php?p=6930#p6930
+
+@echo off
+SET PYTHONIOENCODING=UTF-8
+python distortion_analyzer_gui.py %*
+pause
