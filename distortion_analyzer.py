FFT improvements (RMS calculation) and added 8588A to list of available multimeters + Improved FFT and RMS calculations. Now actually correct for FFT length appropriately and apply amplitude correction after windowing + Added Fluke 8588A to the list of available DMM's under the multimeter tab

distortion_analyzer.py
@@ -281,26 +281,6 @@ class DistortionAnalyzer:
         error = params['error']
         filter_val = params['filter']
 
-        # SIGNAL SOURCE ================================================================================================
-        if suffix in ('A', 'a'):
-            if amplitude <= 1.5:
-                oper_range = 10 ** round(np.log10(amplitude))
-            elif 1.5 <= amplitude <= 10:
-                oper_range = 10
-            else:
-                oper_range = 30
-        else:
-            if amplitude <= 0.12:
-                oper_range = 0.1
-            elif amplitude <= 1.2:
-                oper_range = 1
-            elif amplitude <= 12:
-                oper_range = 10
-            elif amplitude <= 120:
-                oper_range = 100
-            else:
-                oper_range = 1000
-
         # DIGITIZED SIGNAL =============================================================================================
         if Ft == 0:
 
@@ -323,8 +303,9 @@ class DistortionAnalyzer:
         # TODO
         # This is for internal debugging only. Not user facing.
         if not self.DUMMY_DATA:
+            # TODO: shouldn't we always want to setup digitizer for new range??
             if setup:
-                self.M.setup_digitizer(suffix, oper_range, filter_val, N, aperture)
+                self.M.setup_digitizer(suffix, amplitude, filter_val, N, aperture)
             if params['source']:
                 try:
                     self.M.run_source(suffix, amplitude, Ft)
@@ -360,8 +341,8 @@ class DistortionAnalyzer:
         time.sleep(1)
 
         # METER
-        self.M.setup_meter('VOLT', 'AC')
-        meter_outval, meter_range, meter_ft = self.M.read_meter('VOLT', 'AC')
+        self.M.setup_f8588A_meter(autorange=True, output='VOLT', mode='AC')
+        meter_outval, meter_range, meter_ft = self.M.read_f8588A_meter()
         meter_mode = 'V'
 
         # DIGITIZER
@@ -458,9 +439,13 @@ class DistortionAnalyzer:
         yt = data['y']
 
         x_periods = 4
-        xt_end = min(x_periods / F0, runtime)
         ylimit = np.max(np.abs(yt)) * 1.25
         yt_tick = ylimit / 4
+
+        xt_left = 0
+        xt_right = min(x_periods / F0, runtime)
+        yt_btm = -ylimit
+        yt_top = ylimit + yt_tick
 
         # SPECTRAL -----------------------------------------------------------------------------------------------------
         xf = data['xf']
@@ -469,15 +454,18 @@ class DistortionAnalyzer:
         N = data['N']
         yrms = data['RMS']
 
-        xf_end = min(10 ** (np.ceil(np.log10(F0)) + 1), Fs / 2 - Fs / N)  # Does not exceed max bin
+        xf_left = np.min(xf)
+        xf_right = min(10 ** (np.ceil(np.log10(F0)) + 1), Fs / 2 - Fs / N)  # Does not exceed max bin
+        yf_btm = -250
+        yf_top = 0
 
         params = {'xt': xt, 'yt': yt,
-                  'xt_start': 0, 'xt_end': 1e3 * xt_end,
-                  'yt_start': -ylimit, 'yt_end': ylimit + yt_tick, 'yt_tick': yt_tick,
+                  'xt_left': xt_left, 'xt_right': 1000 * xt_right,
+                  'yt_btm': yt_btm, 'yt_top': yt_top, 'yt_tick': yt_tick,
 
                   'xf': xf[0:N] / 1000, 'yf': 20 * np.log10(2 * np.abs(yf[0:N] / (yrms * N))),
-                  'xf_start': np.min(xf) / 1000, 'xf_end': xf_end / 1000,
-                  'yf_start': -150, 'yf_end': 0
+                  'xf_left': xf_left / 1000, 'xf_right': xf_right / 1000,
+                  'yf_btm': yf_btm, 'yf_top': yf_top
                   }
 
         self.frame.plot(params)

distortion_calculator.py
@@ -87,6 +87,7 @@ def windowed_fft(yt, Fs, N, windfunc='blackman'):
     # remove DC offset
     yt -= np.mean(yt)
 
+    # Calculate windowing function and its length ----------------------------------------------------------------------
     if windfunc == 'bartlett':
         w = np.bartlett(N)
     elif windfunc == 'hanning':
@@ -98,20 +99,28 @@ def windowed_fft(yt, Fs, N, windfunc='blackman'):
     else:
         w = np.kaiser(N)
 
-    # https://numpy.org/doc/stable/reference/generated/numpy.fft.rfft.html
-    # function does not compute the negative frequency terms
-    yf_fft = np.fft.fft(yt * w)
+    # Calculate amplitude correction factor after windowing ------------------------------------------------------------
+    # https://stackoverflow.com/q/47904399/3382269
+    amplitude_correction_factor = 1 / np.mean(w)
 
+    # Calculate the length of the FFT ----------------------------------------------------------------------------------
     if (N % 2) == 0:
-        # for even values of N: length is (N / 2) + 1
-        length = int(N / 2) + 1
+        # for even values of N: FFT length is (N / 2) + 1
+        fft_length = int(N / 2) + 1
     else:
-        # for odd values of N: length is (N + 1) / 2
-        length = int((N + 2) / 2)
+        # for odd values of N: FFT length is (N + 1) / 2
+        fft_length = int((N + 2) / 2)
 
-    yf_rfft = yf_fft[:length]
+    """
+    Compute the FFT of the signal Divide by the length of the FFT to recover the original amplitude. Note dividing 
+    alternatively by N samples of the time-series data splits the power between the positive and negative sides. 
+    However, we are only looking at one side of the FFT.
+    """
+    yf_fft = (np.fft.fft(yt * w) / fft_length) * amplitude_correction_factor
+
+    yf_rfft = yf_fft[:fft_length]
     xf_fft = np.linspace(0.0, Fs, N)
-    xf_rfft = np.linspace(0.0, Fs/2, length)
+    xf_rfft = np.linspace(0.0, Fs / 2, fft_length)
 
     return xf_fft, yf_fft, xf_rfft, yf_rfft
 

dmm_f8588A.py
@@ -1,21 +1,21 @@
 import VisaClient
 import time
+import numpy as np
 
 DIGITIZER_SAMPLING_FREQUENCY = 5e6
 instruments = {'f8588A': {'address': '10.205.92.156', 'port': '3490', 'gpib': '6', 'mode': 'SOCKET'}}
 
 
 ########################################################################################################################
-def to_float(s):
-    f = 0.0
+def to_float(string_val):
     try:
-        f = float(s)
+        float_val = float(string_val)
     except ValueError:
         print('[ERROR] Measurement could not be converted to float. Possible issues with configuration.')
         raise ValueError('Prospective measurement obtained by the Fluke 8588A could not be converted to float. Suspect '
                          'null value or over-range')
     else:
-        return f
+        return float_val
 
 
 def getSamplingFrequency(F0, bw=100e3):
@@ -88,6 +88,10 @@ class f8588A_instrument:
         self.f8588A_IDN = ''
         self.f8588_connected = False
 
+        self.setup = True
+        self.output = 'VOLT'
+        self.mode = 'DC'
+
     def connect_to_f8588A(self, instr_id):
         # ESTABLISH COMMUNICATION TO INSTRUMENTS -----------------------------------------------------------------------
         self.f8588A = VisaClient.VisaClient(instr_id)  # Fluke 8588A
@@ -103,8 +107,182 @@ class f8588A_instrument:
                   'connected properly or not being used by another remote session. Consider power cycling the '
                   'suspected instrument\n')
 
-    ####################################################################################################################
-    def setup_meter(self, output='CURR', mode='AC'):
+    # SETUP METER ######################################################################################################
+    def setup_f8588A_meter(self, autorange=True, **kwds):
+        """
+        method accepts several keyword pairs as arguments. Specify at least two of the following:
+            + 'output' ('VOLT' or 'CURR') and 'mode' ('AC' or 'DC')
+            + 'output' ('VOLT' or 'CURR') and 'frequency' (a float value >= 0)
+            + 'units' ('A' or 'V') and 'frequency' (a float value >= 0)
+
+        :param autorange: True or False
+        :param kwds: dictionary containing at least two of the following keys: 'output', 'mode', 'units', 'frequency'
+        :return: True if completion successful
+        """
+
+        self.output, self.mode = self._get_function_params(**kwds)
+
+        try:
+            self.f8588A.write(f'CONF:{self.output}:{self.mode}')
+            if autorange:
+                self.f8588A.write(f'{self.output}:{self.mode}:RANGE:AUTO ON')
+            else:
+                # Set Fluke 884xA to largest range for internal protection by default.
+                self.set_f8588A_range(ideal_range_val=1000, output=self.output, mode=self.mode)
+            time.sleep(1)
+            return True
+
+        except Exception as e:
+            print('setup_meter for the Fluke 8588A failed. What error was thrown here?')
+            print(e)
+            raise ValueError('Setting up Fluke 8588A failed.\nCheck connection and configuration to instrument.')
+
+    def get_f8588A_range(self):
+        """
+        A convenience function for determining the appropriate range for a measurement. Use-case is determining the
+        range of an automated measurement without requiring auto-range. This reduces loading non-linearity associated
+        with switching ranges. This method allows the meter to range correctly before performing the measurement.
+        :return:
+        """
+        dmm_range = to_float(self.f8588A.query(f'{self.output}:{self.mode}:RANGE?'))
+        return dmm_range
+
+    def _get_function_params(self, **kwds):
+        """
+        method accepts several keyword pairs as arguments. Specify at least two of the following:
+            + 'output' ('VOLT' or 'CURR') and 'mode' ('AC' or 'DC')
+            + 'output' ('VOLT' or 'CURR') and 'frequency' (a float value >= 0)
+            + 'units' ('A' or 'V') and 'frequency' (a float value >= 0)
+
+        :param kwds: dictionary containing at least two of the following keys: 'output', 'mode', 'units', 'frequency'
+        :return: output and mode used for setting up the Fluke 884xA
+        """
+        # SORT through what keywords were provided =====================================================================
+        keys = kwds.keys()
+
+        # provided output and mode -------------------------------------------------------------------------------------
+        if keys >= {'output', 'mode'}:
+            output = kwds['output']
+            mode = kwds['mode']
+
+        # provided frequency and either output or units ----------------------------------------------------------------
+        elif 'frequency' in keys:
+            if kwds['frequency'] > 0:
+                mode = 'AC'
+            else:
+                mode = 'DC'
+
+            # provided output ------------------------------------------------------------------------------------------
+            if 'output' in keys and kwds['output'] in ('VOLT', 'CURR'):
+                output = kwds['output']
+
+            # provided units -------------------------------------------------------------------------------------------
+            elif 'units' in keys and kwds['units'] in ('A', 'V'):
+                if kwds['units'] == 'V':
+                    output = 'VOLT'
+                else:
+                    output = 'CURR'
+            else:
+                raise ValueError("Could not determine appropriate mode ('VOLT' or 'CURR') for meter from "
+                                 "the provided arguments!")
+        else:
+            raise ValueError("Provided keywords or keyword values are insufficient for determining appropriate output "
+                             "and mode values for the Fluke 8588A configuration!")
+
+        return output, mode
+
+    # RANGE ############################################################################################################
+    def determine_f8588A_range(self, val: float, units: str):
+        # overange is 20%. Max value must be less than (1 + 20%) of nominal range --------------------------------------
+        if units in ("A", "CURR"):
+            if val < 10e-6 * (1 + 0.2):
+                range_val = 10e-6
+                range_string = '10uA'
+            elif val < 100e-6 * (1 + 0.2):
+                range_val = 100e-6
+                range_string = '100uA'
+            elif val < 1e-3 * (1 + 0.2):
+                range_val = 1e-3
+                range_string = '1mA'
+            elif val < 10e-3 * (1 + 0.2):
+                range_val = 10e-3
+                range_string = '10mA'
+            elif val < 100e-3 * (1 + 0.2):
+                range_val = 100e-3
+                range_string = '100mA'
+            elif val < 1 * (1 + 0.2):
+                range_val = 1
+                range_string = '1A'
+            elif val < 10 * (1 + 0.2):
+                range_val = 10
+                range_string = '10A'
+            else:
+                range_val = 30
+                range_string = '30A'
+
+        elif units in ('V', "VOLT"):
+            if val < 0.10 * (1 + 0.2):
+                range_val = 0.1
+                range_string = '100mV'
+            elif val < 1.0 * (1 + 0.2):
+                range_val = 1
+                range_string = '1V'
+            elif val < 10 * (1 + 0.2):
+                range_val = 10
+                range_string = '10V'
+            elif val < 100 * (1 + 0.2):
+                range_val = 100
+                range_string = '100V'
+            else:
+                range_val = 1000
+                range_string = '1000V'
+        else:
+            range_val = 1000
+            range_string = '1000V'
+            print("Failed to find appropriate range. Setting to default.")
+
+        return range_val, range_string
+
+    def set_f8588A_range(self, ideal_range_val, **kwds):
+        """
+        method accepts several keyword pairs as arguments. Specify at least two of the following:
+            + 'output' ('VOLT' or 'CURR') and 'mode' ('AC' or 'DC')
+            + 'output' ('VOLT' or 'CURR') and 'frequency' (a float value >= 0)
+            + 'units' ('A' or 'V') and 'frequency' (a float value >= 0)
+
+        Note: Meter displays overload and sends 9.9000 E+37 over the remote interface when input signal is greater
+        than the selected range can measure.
+
+        :param ideal_range_val: user defined range to set Fluke 884xA to
+        :param kwds: dictionary containing at least two of the following keys: 'output', 'mode', 'units', 'frequency'
+        :return: True iff range is set successfully
+        """
+        # Get function parameters for Fluke 884xA ----------------------------------------------------------------------
+        output, mode = self._get_function_params(**kwds)  # ('VOLT', 'AC')
+
+        # Check if output and mode values have changed since setup. They shouldn't. ------------------------------------
+        if output != self.output:
+            self.output = output
+        if mode != self.mode:
+            self.mode = mode
+
+        # Causes the meter to exit autoranging on the primary display and enter manual ranging. The present range ------
+        # becomes the selected range. ----------------------------------------------------------------------------------
+        self.f8588A.write(f"{self.output}:{self.mode}:FIXED")
+
+        # Calculate the closest range for measurement ------------------------------------------------------------------
+        range_val, range_string = self.determine_f8588A_range(ideal_range_val, self.output)
+
+        # Set new range ------------------------------------------------------------------------------------------------
+        try:
+            self.f8588A.write(f"{self.output}:{self.mode}:RANGE {range_val}")
+            print(f"Successfully set range of Fluke 884xA to {range_val} ({range_string})")
+            return True
+        except Exception:
+            raise ValueError(f"Failed to set range to {range_val} ({range_string})")
+
+    # RETRIEVE MEASUREMENT #############################################################################################
+    def read_f8588A_meter(self):
         """
         The secondary readings available for AC voltage and current are: “FREQuency”,
         “PERiod”, "PK to Pk", "Crest Factor", "Pos Peak", "Neg peak", “OFF” (turns off
@@ -112,48 +290,66 @@ class f8588A_instrument:
         the secondary reading can be "Shunt Voltage" or "Power coefficient". See
         FETCh?.
 
-        :param output: CURRent or VOLTage
-        :param mode: AC or DC
-        :return:
+        :return: primary and secondary displayed values, and RANGE
         """
-        self.f8588A.write(f'CONF:{output}:{mode}')
-        self.f8588A.write(f'{output}:{mode}:RANGE:AUTO ON')
-        time.sleep(1)
+        if self.setup:
+            freqval = 0.0
+            time.sleep(1)
+            self.f8588A.write('INIT:IMM')
 
-    def read_meter(self, output, mode):
+            # Primary result = 1 (page 17 of 8588A's programmers manual)
+            # A return of 9.91E+37 indicates there is not a valid value to return (NaN - not a number)
+            # time delay prevents NaN result
+            time.sleep(0.2)
+            outval = to_float(self.f8588A.query('FETCH? 1'))
+            dmm_range = self.get_f8588A_range()
+
+            if self.mode == 'AC':
+                # FREQuency = 2 (page 17 of 8588A's programmers manual)
+                freqval = to_float(self.f8588A.query('FETCH? 2'))
+
+            return outval, freqval, dmm_range
+        else:
+            raise ValueError('Fluke 8588A has not been configured for measurement properly or has been disconnected.')
+
+    def average_f8588A_reading(self, samples=10, dt=0.1):
+        readings = np.zeros(samples)
         freqval = 0.0
-        time.sleep(1)
-        self.f8588A.write('INIT:IMM')
 
-        # Primary result = 1 (page 17 of 8588A's programmers manual)
-        # A return of 9.91E+37 indicates there is not a valid value to return (NaN - not a number)
-        # time delay prevents NaN result
-        time.sleep(0.2)
-        outval = to_float(self.f8588A.query('FETCH? 1'))
-        dmm_range = to_float(self.f8588A.query(f'{output}:{mode}:RANGE?'))
+        for idx in range(samples):
+            readings[idx], freqval, dmm_range = self.read_f8588A_meter()
+            time.sleep(dt)
 
-        if mode == 'AC':
-            # FREQuency = 2 (page 17 of 8588A's programmers manual)
-            freqval = to_float(self.f8588A.query('FETCH? 2'))
+        mean = readings.mean()
+        std = np.sqrt(np.mean(abs(readings - mean) ** 2))
 
-        return outval, dmm_range, freqval
+        return mean, freqval, std
 
-    ####################################################################################################################
-    def setup_digitizer(self, mode, oper_range, filter_val, N, aperture):
+    # DIGITIZER ########################################################################################################
+    def setup_digitizer(self, mode, ideal_range_val, filter_val, N, aperture):
         # f8588A has a 5MHz sampled rate clock. adjusting aperture time, averages more points, which adjusts sample rate
         self.f8588A.write('*RST')
-        if mode in ('A', 'a'):
-            self.f8588A.write(':FUNC "DIGitize:CURRent" ')
-            self.f8588A.write(f':DIGitize:CURRent:RANGe {oper_range}')
-        else:
-            self.f8588A.write(':FUNC "DIGitize:VOLTage" ')
-            self.f8588A.write(f':DIGitize:VOLTage:RANGe {oper_range}')
-        self.f8588A.write(f':DIGitize:FILTer {filter_val}')
-        self.f8588A.write(f':DIGitize:APERture {aperture}')
-        self.f8588A.write('TRIGger:RESet')
-        self.f8588A.write(f'TRIGGER:COUNT {N}')
-        self.f8588A.write('TRIGger:DELay:AUTO OFF')
-        self.f8588A.write('TRIGGER:DELay 0')
+
+        # Calculate the closest range for measurement ------------------------------------------------------------------
+        range_val, range_string = self.determine_f8588A_range(ideal_range_val, mode)  # (0.1, '0.1A')
+        try:
+            self.f8588A.write(f':FUNC "DIGitize:{self.mode}" ')
+            self.f8588A.write(f':DIGitize:{self.mode}:RANGe {range_val}')
+            print(f"Successfully set range of Fluke 8588A to {range_string}")
+
+            self.f8588A.write(f':DIGitize:FILTer {filter_val}')
+            self.f8588A.write(f':DIGitize:APERture {aperture}')
+            self.f8588A.write('TRIGger:RESet')
+            self.f8588A.write(f'TRIGGER:COUNT {N}')
+            self.f8588A.write('TRIGger:DELay:AUTO OFF')
+            self.f8588A.write('TRIGGER:DELay 0')
+            return True  # returns true if digitizer setup completes succesfully
+
+        except Exception as e:
+            print('setup_digitizer for the Fluke 8588A failed. What error was thrown here?')
+            print(e)
+            raise ValueError('Setting up digitizer for Fluke 8588A failed.'
+                             '\nCheck connection and configuration to instrument.')
 
     def retrieve_digitize(self):
         self.f8588A.write('INIT:IMM')
@@ -172,13 +368,14 @@ class f8588A_instrument:
 
 # Run
 if __name__ == "__main__":
-    output, mode = 'VOLT', 'AC'
     instr = f8588A_instrument()
-
     instr.connect_to_f8588A(instruments)
-    instr.setup_meter(output, mode)
 
-    outval, dmm_range, freqval = instr.read_meter(mode)
+    # 1. Setup the meter for measurement
+    instr.setup_f8588A_meter(autorange=True, output='VOLT', mode='AC')
+    # 2. Get Average Reading
+    outval, freqval, std = instr.average_f8588A_reading(samples=10, dt=0.1)
+
     print(f"\nOutput: {outval}\nFrequency: {freqval} Hz")
 
     instr.close_f8588A()

dmm_f884xA.py
@@ -7,18 +7,18 @@ INFO = "file:///C:/Users/rholle/AppData/Local/Temp/8845A___pmeng0300.pdf"
 
 
 ########################################################################################################################
-def to_float(s):
+def to_float(string_val):
     try:
-        f = float(s)
+        float_val = float(string_val)
     except ValueError:
         print('[ERROR] Measurement could not be converted to float. Possible issues with configuration.')
         raise ValueError('Prospective measurement obtained by the Fluke 884xA could not be converted to float. Suspect '
                          'null value or over-range')
     else:
-        return f
+        return float_val
 
 
-class f884xA_instruments:
+class f884xA_instrument:
 
     def __init__(self):
         self.measurement = []
@@ -45,29 +45,37 @@ class f884xA_instruments:
                   'connected properly or not being used by another remote session. Consider power cycling the '
                   'suspected instrument\n')
 
-    ####################################################################################################################
-    def f884xA_meter_setup(self, autorange=True, **kwds):
-        output, mode = self._get_function_params(**kwds)
-        self.output = output
-        self.mode = mode
+    # SETUP METER ######################################################################################################
+    def setup_f884xA_meter(self, autorange=True, **kwds):
+        """
+        method accepts several keyword pairs as arguments. Specify at least two of the following:
+            + 'output' ('VOLT' or 'CURR') and 'mode' ('AC' or 'DC')
+            + 'output' ('VOLT' or 'CURR') and 'frequency' (a float value >= 0)
+            + 'units' ('A' or 'V') and 'frequency' (a float value >= 0)
+
+        :param autorange: True or False
+        :param kwds: dictionary containing at least two of the following keys: 'output', 'mode', 'units', 'frequency'
+        :return: True if completion successful
+        """
+        self.output, self.mode = self._get_function_params(**kwds)
 
         try:
-            self.f884xA.write(f'CONF:{output}:{mode}')
+            self.f884xA.write(f'CONF:{self.output}:{self.mode}')
             if autorange:
-                self.f884xA.write(f'{output}:{mode}:RANGE:AUTO ON')
+                self.f884xA.write(f'{self.output}:{self.mode}:RANGE:AUTO ON')
             else:
                 # Set Fluke 884xA to largest range for internal protection by default.
-                self.set_range(ideal_range_val=1000, output=output, mode=mode)
+                self.set_f884xA_range(ideal_range_val=1000, output=self.output, mode=self.mode)
             time.sleep(1)
             self.setup = True
-            return True
+            return True  # returns true if setup completes succesfully
 
         except Exception as e:
-            print('setup_meter for 884xA failed. What error was thrown here?')
+            print('setup_meter for the Fluke 884xA failed. What error was thrown here?')
             print(e)
-            raise ValueError('Setting up Fluke 884xA failed. Check connection and configuration to instrument.')
+            raise ValueError('Setting up Fluke 884xA failed.\nCheck connection and configuration to instrument.')
 
-    def get_range(self):
+    def get_f884xA_range(self):
         """
         A convenience function for determining the appropriate range for a measurement. Use-case is determining the
         range of an automated measurement without requiring auto-range. This reduces loading non-linearity associated
@@ -137,7 +145,8 @@ class f884xA_instruments:
 
         return output, mode
 
-    def determine_range(self, val: float, output: str):
+    # RANGE ############################################################################################################
+    def determine_f884xA_range(self, val: float, units: str):
         # Set multiplier based on measurement rate ---------------------------------------------------------------------
         rate = self.get_rate()
 
@@ -149,7 +158,7 @@ class f884xA_instruments:
             multiplier = 3
 
         # overange is 20%. Max value must be less than (1 + 20%) of nominal range --------------------------------------
-        if output in ("A", "CURR"):
+        if units in ("A", "CURR"):
             if val < 10e-3 * multiplier * (1 + 0.2):
                 range_val = 1
                 range_string = '30mA'
@@ -160,7 +169,7 @@ class f884xA_instruments:
                 range_val = 3
                 range_string = '10A'
 
-        elif output in ('V', "VOLT"):
+        elif units in ('V', "VOLT"):
             if val < 100e-3 * multiplier * (1 + 0.2):
                 range_val = 1
                 range_string = '100mV'
@@ -183,7 +192,7 @@ class f884xA_instruments:
 
         return range_val, range_string
 
-    def set_range(self, ideal_range_val, **kwds):
+    def set_f884xA_range(self, ideal_range_val, **kwds):
         """
         method accepts several keyword pairs as arguments. Specify at least two of the following:
             + 'output' ('VOLT' or 'CURR') and 'mode' ('AC' or 'DC')
@@ -198,7 +207,7 @@ class f884xA_instruments:
         :return: True iff range is set successfully
         """
         # Get function parameters for Fluke 884xA ----------------------------------------------------------------------
-        output, mode = self._get_function_params(**kwds)
+        output, mode = self._get_function_params(**kwds)  # ('VOLT', 'AC')
 
         # Check if output and mode values have changed since setup. They shouldn't. ------------------------------------
         if output != self.output:
@@ -211,7 +220,7 @@ class f884xA_instruments:
         self.f884xA.write(f"{self.output}:{self.mode}:FIXED")
 
         # Calculate the closest range for measurement ------------------------------------------------------------------
-        range_val, range_string = self.determine_range(ideal_range_val, self.output)
+        range_val, range_string = self.determine_f884xA_range(ideal_range_val, self.output)
 
         # Set new range ------------------------------------------------------------------------------------------------
         try:
@@ -221,31 +230,33 @@ class f884xA_instruments:
         except Exception:
             raise ValueError(f"Failed to set range to {range_val} ({range_string})")
 
-    ####################################################################################################################
-    def read_meter(self):
+    # RETRIEVE MEASUREMENT #############################################################################################
+    def read_f884xA_meter(self):
         if self.setup:
             freqval = 0.0
             time.sleep(1)
-            self.f884xA.write('INIT:IMM')
+            # Initiate Triggering - (MEASure? or READ? or INITiate)
+            self.f884xA.write('INIT')
             time.sleep(0.2)
 
             # FETCh1? Returns measurements from the primary display
             outval = to_float(self.f884xA.query('FETCh1?'))
+            dmm_range = self.get_f884xA_range()
 
             if self.mode == 'AC':
                 # FETCh2? Returns readings from the secondary display
                 freqval = to_float(self.f884xA.query('FETCh2?'))
 
-            return outval, freqval
+            return outval, freqval, dmm_range
         else:
             raise ValueError('Fluke 884xA has not been configured for measurement.')
 
-    def average_reading(self, N, dt=0.1):
-        readings = np.zeros(N)
+    def average_f884xA_reading(self, samples=10, dt=0.1):
+        readings = np.zeros(samples)
         freqval = 0.0
 
-        for idx in range(N):
-            readings[idx], freqval = self.read_meter()
+        for idx in range(samples):
+            readings[idx], freqval, dmm_range = self.read_f884xA_meter()
             time.sleep(dt)
 
         mean = readings.mean()
@@ -263,12 +274,14 @@ class f884xA_instruments:
 
 # Run
 if __name__ == "__main__":
-    instr = f884xA_instruments()
+    instr = f884xA_instrument()
     instr.connect_to_f884xA(instruments)
 
-    autorange = True
-    instr.f884xA_meter_setup(autorange=autorange, output='VOLT', mode='AC')
-    outval, freqval, std = instr.average_reading(10)
+    # 1. Setup the meter for measurement
+    instr.setup_f884xA_meter(autorange=True, output='VOLT', mode='AC')
+    # 2. Get Average Reading
+    outval, freqval, std = instr.average_f884xA_reading(samples=10, dt=0.1)
+
     print(f"\nOutput: {outval}\nFrequency: {freqval} Hz")
 
     instr.close_f884xA()

gui_panel_distortion_analyzer.py
@@ -409,14 +409,14 @@ class DistortionAnalyzerTab(wx.Panel):
 
         self.temporal.set_data(xt, yt)
 
-        xt_start = params['xt_start']
-        xt_end = params['xt_end']
-        yt_start = params['yt_start']
-        yt_end = params['yt_end']
+        xt_left = params['xt_left']
+        xt_right = params['xt_right']
+        yt_btm = params['yt_btm']
+        yt_top = params['yt_top']
         yt_tick = params['yt_tick']
 
-        self.ax1.set_xlim([xt_start, xt_end])
-        self.ax1.set_yticks(np.arange(yt_start, yt_end, yt_tick))
+        self.ax1.set_xlim(left=xt_left, right=xt_right)
+        self.ax1.set_yticks(np.arange(yt_btm, yt_top, yt_tick))
 
         # SPECTRAL -----------------------------------------------------------------------------------------------------
         xf = params['xf']
@@ -424,13 +424,13 @@ class DistortionAnalyzerTab(wx.Panel):
 
         self.spectral.set_data(xf, yf)
 
-        xf_start = params['xf_start']
-        xf_end = params['xf_end']
-        yf_start = params['yf_start']
-        yf_end = params['yf_end']
+        xf_left = params['xf_left']
+        xf_right = params['xf_right']
+        yf_btm = params['yf_btm']
+        yf_top = params['yf_top']
 
-        self.ax2.set_xlim(xf_start, xf_end)
-        self.ax2.set_ylim(yf_start, yf_end)
+        self.ax2.set_xlim(left=xf_left, right=xf_right)
+        self.ax2.set_ylim(bottom=yf_btm, top=yf_top)
 
         # REDRAW PLOT --------------------------------------------------------------------------------------------------
         self.plot_redraw()
@@ -443,7 +443,8 @@ class DistortionAnalyzerTab(wx.Panel):
             yt_length = len(self.ax1.get_ydata())
             print(f'Are the lengths of xt: {xt_length} and yt: {yt_length} mismatched?')
             raise
-        self.ax1.autoscale()
+        self.ax1.margins(x=0)
+        self.ax1.autoscale(axis='y')
 
         # UPDATE PLOT FEATURES -----------------------------------------------------------------------------------------
         self.figure.tight_layout()

multimeter.py
@@ -1,5 +1,6 @@
 import dut_f5560A as dut
-import dmm_f884xA as dmm
+import dmm_f884xA as dmm1
+import dmm_f8588A as dmm2
 
 import time
 import pandas as pd
@@ -38,12 +39,13 @@ def write_to_csv(path, fname, header, *args):
 
 
 ########################################################################################################################
-class Instruments(dut.f5560A_instrument, dmm.f884xA_instruments):
+class Instruments(dut.f5560A_instrument, dmm1.f884xA_instrument, dmm2.f8588A_instrument):
     def __init__(self, parent):
         dut.f5560A_instrument.__init__(self)
-        dmm.f884xA_instruments.__init__(self)
+        dmm1.f884xA_instrument.__init__(self)
+        dmm2.f8588A_instrument.__init__(self)
 
-        self.analyzer = parent
+        self.parent = parent
         self.measurement = []
         self.connected = False
 
@@ -51,16 +53,22 @@ class Instruments(dut.f5560A_instrument, dmm.f884xA_instruments):
         try:
             # ESTABLISH COMMUNICATION TO INSTRUMENTS -------------------------------------------------------------------
             f5560A_id = instruments['f5560A']
-            f884xA_id = instruments['f884xA']
+            DMM_id = instruments[self.parent.DMM_choice]
 
             self.connect_to_f5560A(f5560A_id)
-            self.connect_to_f884xA(f884xA_id)
+            if self.parent.DMM_choice == 'f884xA':
+                self.connect_to_f884xA(DMM_id)
+            elif self.parent.DMM_choice == 'f8588A':
+                self.connect_to_f8588A(DMM_id)
+            else:
+                raise ValueError("Invalid DMM choice selected!")
 
-            if self.f5560A.healthy and self.f884xA.healthy:
+            if self.f5560A.healthy and (self.f884xA.healthy or self.f8588A.healthy):
                 self.connected = True
                 try:
                     idn_dict = {'DUT': self.f5560A_IDN, 'DMM': self.f884xA_IDN}
-                    self.analyzer.parent.set_ident(idn_dict)
+                    # TODO: parent.parent. --nice
+                    self.parent.parent.set_ident(idn_dict)
                     self.setup_source()
                 except ValueError:
                     raise
@@ -72,7 +80,13 @@ class Instruments(dut.f5560A_instrument, dmm.f884xA_instruments):
     def close_instruments(self):
         time.sleep(1)
         self.close_f5560A()
-        self.close_f884xA()
+        if self.parent.DMM_choice == 'f884xA':
+            self.close_f884xA()
+        elif self.parent.DMM_choice == 'f8588A':
+            self.close_f8588A()
+        else:
+            raise ValueError("Failed to close DMM instrument!"
+                             "\nThe DMM selected may not match what is currently in remote.")
         self.connected = False
 
 
@@ -84,6 +98,7 @@ class DMM_Measurement:
         self.amplitude_good = False  # Flag indicates user input for amplitude value is good (True)
         self.frequency_good = False  # Flag indicates user input for frequency value is good (True)
 
+        self.DMM_choice = 'f8588A'
         self.params = {'autorange': True, 'always_voltage': True,
                        'mode': 0, 'amplitude': '', 'units': '',
                        'rms': 0, 'frequency': 0.0,
@@ -251,22 +266,33 @@ class DMM_Measurement:
 
             # Usually enters on each new measurement, but only once for each loop (measurement sweep) ------------------
             if setup:
-                self.M.f884xA_meter_setup(autorange=autorange, units=dmm_units, frequency=Ft)
+                self.M.setup_f884xA_meter(autorange=autorange, units=dmm_units, frequency=Ft)
                 self._Ft = Ft
 
             # During a loop (measurement sweep) we want to catch changes from either AC to DC or vice-versa ------------
             elif (Ft * self._Ft) == 0 and Ft != self._Ft:
-                self.M.f884xA_meter_setup(autorange=autorange, units=dmm_units, frequency=Ft)
+                self.M.setup_f884xA_meter(autorange=autorange, units=dmm_units, frequency=Ft)
                 self._Ft = Ft
 
             try:
                 if not autorange:
-                    self.M.set_range(ideal_range_val=amplitude, units=units, frequency=Ft)
+                    if self.DMM_choice == 'f884xA':
+                        self.M.set_f884xA_range(ideal_range_val=amplitude, units=units, frequency=Ft)
+                    elif self.DMM_choice == 'f8588A':
+                        self.M.set_f8588A_range(ideal_range_val=amplitude, units=units, frequency=Ft)
+                    else:
+                        raise ValueError("Invalid DMM selection made!")
+
                     time.sleep(1)
                 self.M.run_source(units, amplitude, Ft)
 
                 try:
-                    outval, freqval, std = self.M.average_reading(10)
+                    if self.DMM_choice == 'f884xA':
+                        outval, freqval, std = self.M.average_f884xA_reading(10)
+                    elif self.DMM_choice == 'f8588A':
+                        outval, freqval, std = self.M.average_f8588A_reading(10)
+                    else:
+                        raise ValueError("Invalid DMM selection made!")
 
                 except ValueError:
                     print('error occurred while connecting to DMM. Placing Fluke 5560A in Standby.')
@@ -279,7 +305,13 @@ class DMM_Measurement:
         else:
             # DUMMY_DATA for random number generation
             freqval, outval, std = Ft, (7 + 1 * np.random.random(1))[0], (0.25 + np.random.random(1))[0]
-            M = dmm.f884xA_instruments()
+            if self.DMM_choice == 'f884xA':
+                M = dmm1.f884xA_instrument()
+            elif self.DMM_choice == 'f8588A':
+                M = dmm2.f8588A_instrument()
+            else:
+                raise ValueError("Invalid DMM selection made!")
+
             print(f"Fluke 5560A operating units: {units}")
             print(f"Retrieve function parameters for DMM: {M._get_function_params(frequency=freqval, units='A')}")
             print(f"Measured units: {dmm_units}")